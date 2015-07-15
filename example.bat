be9e8c95bc703813eaf080efc5a6640f7438dc6b
Rav3nPL <rav3n.pl@gmail.com>
2015-07-15 22:05:47 +0200

    always submit stale for P2Pool
    For P2Pool mining not submitting stale shares are totally wrong.
    Stale share can be a block solution.

--- example.bat
+++ example.bat
@@ -9,6 +9,6 @@ setx GPU_MAX_ALLOC_PERCENT 100
 del *.bin
 
 
-sgminer.exe --no-submit-stale --kernel Lyra2RE -o stratum+tcp://92.27.201.170:9174 -u m -p 1 --gpu-platform 2 -I 19 --shaders 2816  -w 64 -g 2 
+sgminer.exe --kernel Lyra2RE -o stratum+tcp://92.27.201.170:9174 -u m -p 1 --gpu-platform 2 -I 19 --shaders 2816  -w 64 -g 2 
 
 pause
